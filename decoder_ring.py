64f13cd2f28db2fc31efd36459e418aaecefe52e
David Ellison <davidehellison@gmail.com>
2013-03-16 17:17:40 -0700

    initial commit of decoder ring for "The Boy Detective Fails"

---
+++ decoder_ring.py
@@ -0,0 +1,40 @@
+import itertools
+import re
+import sys
+from collections import defaultdict
+
+class DecoderRing(defaultdict):
+    def __missing__(self, key):
+        return key
+
+letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = range(1, 27)
+
+inF = open(sys.argv[1])
+
+lines = inF.readlines()
+
+key = (lines[0].split(" ")[0], int(lines[0].split(" ")[1]))
+message = lines[1].rstrip()
+
+inF.close()
+
+ring = DecoderRing()
+
+letter_ring = itertools.cycle(letters)
+number_ring = itertools.cycle(numbers)
+
+ring[str(key[1])] = key[0]
+
+while key[0] != letter_ring.next():
+    continue
+while key[1] != number_ring.next():
+    continue
+
+for i in range(26):
+    ring[str(number_ring.next())] = letter_ring.next()
+
+pattern = re.compile(r'\b(' + '|'.join(ring.keys()) + r')\b')
+result = pattern.sub(lambda x: ring[x.group()], message)
+
+print re.sub('-', '', result)
